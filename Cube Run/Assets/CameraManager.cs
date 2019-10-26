411c199d42fe970537cff6995b540e3978c85964
AshwinDevelops <chugh.ashwin@gmail.com>
2019-10-26 12:18:11 -0700

    Functionalities Added
    Camera now follows the player properly. Added a sample obstacle alongside a few new materials to color the map. Tweaked a few rigidbody features to make the game more realistic.

--- Cube Run/Assets/CameraManager.cs
+++ Cube Run/Assets/CameraManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    public GameObject MainPlayer;
+    public Camera MainCamera;
+    public float offsetZ;//set the z distance away from the main player
+    public float offsetY;//set the y distance away from the player
+
+    private Vector3 CameraRealTimePosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CameraRealTimePosition = new Vector3(MainPlayer.transform.position.x, MainPlayer.transform.position.y+offsetY, MainPlayer.transform.position.z-offsetZ);//set the camera's real time position
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate is called every 200 ms(keep the camera in line with player movement)
+    void FixedUpdate()
     {
-        
+        CameraRealTimePosition.x = MainPlayer.transform.position.x;//update X pos
+        CameraRealTimePosition.y = MainPlayer.transform.position.y+offsetY;//update y pos
+        CameraRealTimePosition.z = MainPlayer.transform.position.z - offsetZ;//update z pos
+        MainCamera.transform.position = CameraRealTimePosition;//set actual camera position to calculated position
     }
 }
