3564c764f6718a8c927eb81fc795d7a8b71315be
100024229 <56274972+100024229@users.noreply.github.com>
2019-11-04 12:46:23 -0800

    GlobalDataLibrary added

--- Cube Run/Assets/Scenes/MainPlayerManager.cs
+++ Cube Run/Assets/Scenes/MainPlayerManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MainPlayerManager : MonoBehaviour
 {
     public Rigidbody rb;
-    public int speedMultiplier;
+    public int speedMultiplier = GlobalDataLibrary.speedMultiplier;
 
     // Start is called before the first frame update
     void Start()
