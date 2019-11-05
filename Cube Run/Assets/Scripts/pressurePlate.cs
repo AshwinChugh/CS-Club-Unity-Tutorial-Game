c61de6f3b810f2d52391a3c7d2603054699c600f
AshwinDevelops <chugh.ashwin@gmail.com>
2019-11-04 21:39:43 -0800

    Fixed Power Up Boost

---
+++ Cube Run/Assets/Scripts/pressurePlate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pressurePlate : MonoBehaviour
+{
+    public float boostMultiplier;
+
+    private Rigidbody MainPlayerRB;
+    private bool initializedCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainPlayerRB = GameObject.Find("MainPlayer").GetComponent<Rigidbody>();
+        if (MainPlayerRB == null)
+            initializedCheck = false;
+        else
+            initializedCheck = true;
+    }
+
+    void OnTriggerStay(Collider collider) //collidion detect
+    {
+        if (initializedCheck)
+        {
+            MainPlayerRB.AddForce(0, 0, boostMultiplier, ForceMode.Force);
+            //Debug.Log("Force boost");  -->debugging purposes 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
